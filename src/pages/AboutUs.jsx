b4a695e356bbefad60c4d4624b374503b4c316a2
Nhân Nguyễn <91769390+nhannguyen2005@users.noreply.github.com>
2026-04-14 16:02:41 +0700

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,1 +1,12 @@
+function AboutUs() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Giới thiệu</h1>
+      <p>
+        Đây là hệ thống đặt sân bóng giúp người dùng đặt sân nhanh chóng và tiện lợi.
+      </p>
+    </div>
+  );
+}
 
+export default AboutUs;
